a9ba38f6f8fefcf92b3012e20266deebeefdd70a
dprose9 <davidparkerrose@gmail.com>
2026-05-23 14:33:43 -0600

    javascript testing

--- lol.js
+++ lol.js
@@ -1,15 +1,1 @@
-function sizeiframe() {
-	const frme = document.getElementById("urmomid");
-	const doc = frme.contentWindow.document;
-	
-	frme.style.height = doc.body.scrollHeight +"px";
-	frme.style.width = doc.body.scrollWidth +"px";
-}
-
-window.addEventListener("load", () => {
-	const frme = document.getElementById("urmomid");
-	frme.onload = () => {
-		sizeiframe();
-		console.log("urmom menu loaded");
-	};
-});
+document.getElementById("urmomid").innerHTML = "<h1>Urmom Test</h1>";
