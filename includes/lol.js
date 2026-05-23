ac39431a86167119c3c4d5be5a8fb577581f9cd7
dprose9 <davidparkerrose@gmail.com>
2026-05-23 16:11:11 -0600

    javascript testing

--- includes/lol.js
+++ includes/lol.js
@@ -1,5 +1,7 @@
 const menuhtm = await fetch("/includes/menu.htm").then(r => r.text());
 const foothtm = await fetch("/includes/foot.htm").then(r => r.text());
+const verhtm = await fetch("/includes/ver.htm").then(r => r.text());
 
 document.getElementById("menuid").innerHTML = menuhtm;
 document.getElementById("footid").innerHTML = foothtm;
+document.getElementById("verid").innerHTML = verhtm;
